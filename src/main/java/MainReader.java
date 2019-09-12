923970d72eb893291436b3b784c8a1b58a184841
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2019-09-12 20:39:19 -0300

    :bug: Problema com a leiura de espaços.

--- src/main/java/MainReader.java
+++ src/main/java/MainReader.java
@@ -21,10 +21,120 @@ public class MainReader {
         LinkedHashSet<String> tabelaDeSimbolos = new LinkedHashSet<String>();
 
         for (Token t : tokens) {
-            if (t.getType() == scanner.Var) {
-                System.out.println(t.getText());
-                
+            System.out.println(t.getText());
+            if (t.getType() == scanner.ABS) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.EXT) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.PRI) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.PRO) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.PUB) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.CLA) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.THIS) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.NEW) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.NUL) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.IMP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.PAC) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.RET) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.SUP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.IF) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.WHI) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.ELS) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.INS) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.INT) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.CHA) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.VOI) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.BOO) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.STA) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.FAL) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.TRU) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.Atrib) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.AtrArit) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.OpBool) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.OpUni) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.OpArit) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.VOP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.VCL) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.KOP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.KCL) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.AP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.FP) {
+                tabelaDeSimbolos.add(t.getText());
+
+            } else if (t.getType() == scanner.Var) {
+                tabelaDeSimbolos.add(t.getText());
+
             }
+
+        }
+        for (String s : tabelaDeSimbolos) {
+            System.out.println("TS " + s);
         }
     }
 }
